8d994a192f10dc571fce801ec5d50b8a43d85487
José Lino santos García <191011022@acatlan.tecnm.mx>
2024-02-05 19:05:31 -0600

    datos-de-programacion

--- src/app/datos-de-programacion/datos-de-programacion.component.ts
+++ src/app/datos-de-programacion/datos-de-programacion.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 
+
+export interface PeriodicElement {
+  Elemento: string;
+  Cantidad: number;
+  Descripcion: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {Elemento: 'Set 1', Cantidad:1, Descripcion: 'Set 1 para quirófano'},
+  {Elemento: 'Set 2', Cantidad:1, Descripcion: 'Set 2 para quirófano'},
+  {Elemento: 'Set 3', Cantidad:1, Descripcion: 'Set 3 para quirófano'},
+  {Elemento: 'Set 4', Cantidad:1, Descripcion: 'Set 4 para quirófano'},
+];
+
+
 @Component({
   selector: 'app-datos-de-programacion',
   templateUrl: './datos-de-programacion.component.html',
   styleUrl: './datos-de-programacion.component.css'
 })
+
 export class DatosDeProgramacionComponent {
   labelPosition: 'media' | 'alta' = 'alta';
   value = 'Clear me';
 
+
+  displayedColumns: string[] = ['Elemento', 'Cantidad', 'Descripcion'];
+  dataSource = ELEMENT_DATA;
 
 }
