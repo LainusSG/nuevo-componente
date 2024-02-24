e48edf42800261a7de1b5c946d1cb9dcd0861467
LainusSG <191011022@acatlan.tecnm.mx>
2024-02-24 16:19:35 -0600

    Emergente-recepcion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BotonComponent } from './Componente/boton/boton.component';
+import { EmergenteComponent } from './Componente/emergente/emergente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BotonComponent,
+    EmergenteComponent
   ],
   imports: [
     BrowserModule,
