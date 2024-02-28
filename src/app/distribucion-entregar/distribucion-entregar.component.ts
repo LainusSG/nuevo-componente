b88c57756cb71e1fb292696016f3d55de2a7a17a
LainusSG <191011022@acatlan.tecnm.mx>
2024-02-27 23:51:35 -0600

    Distribucion-Entregar

---
+++ src/app/distribucion-entregar/distribucion-entregar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-distribucion-entregar',
+  templateUrl: './distribucion-entregar.component.html',
+  styleUrl: './distribucion-entregar.component.css'
+})
+export class DistribucionEntregarComponent {
+
+}
